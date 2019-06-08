704a2fbad042d0361fb0c5ddbd8e9e1c80cd5072
Jane <44191210+zhouzhouyh2018@users.noreply.github.com>
2019-06-09 00:41:23 +0800

    Add files via upload

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,42 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/todo_todoIndex_0__0_phone.png",
+        "test": "../bitmaps_test/20190607-155759/todo_todoIndex_0__0_phone.png",
+        "selector": "",
+        "fileName": "todo_todoIndex_0__0_phone.png",
+        "label": "todoIndex",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:8080/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "engineErrorMsg": "Navigation Timeout Exceeded: 60000ms exceeded",
+        "error": "Reference file not found /home/xjtuse/nightwatch/react-todo-app/backstop_data/bitmaps_reference/todo_todoIndex_0__0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/todo_todoIndex_0__1_tablet.png",
+        "test": "../bitmaps_test/20190607-155759/todo_todoIndex_0__1_tablet.png",
+        "selector": "",
+        "fileName": "todo_todoIndex_0__1_tablet.png",
+        "label": "todoIndex",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:8080/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "engineErrorMsg": "Navigation Timeout Exceeded: 60000ms exceeded",
+        "error": "Reference file not found /home/xjtuse/nightwatch/react-todo-app/backstop_data/bitmaps_reference/todo_todoIndex_0__1_tablet.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "todo"
+});
